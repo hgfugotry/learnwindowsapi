87bf3e862c48e7b3b52cf7b975bbb3caee00bae4
hgfugotry <hgfugotry@outlook.com>
2024-08-07 16:03:12 +0800

    auto save

--- autowrite.cpp
+++ autowrite.cpp
@@ -1,6 +1,9 @@
+#include<iostream>
 #include<cstdlib>
 int main()
 {
+    system("g:");
+    system("cd G:\\cpp\\learnwindowsapi");
     system("git add .");
     system("git commit -m \"auto save\"");
     system("git push 1 main");
